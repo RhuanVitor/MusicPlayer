27fd76eaf0aeba3065ab693e9ee0ef40e787a0fb
Rhuan Vitor <rhuanvs72840@gmail.com>
2024-03-02 14:23:52 -0300

    tempo da music adicionado

--- script.js
+++ script.js
@@ -1,16 +1,30 @@
 const music = document.querySelector("audio");
-const playMusic = () => {
-  music.play()
-  document.querySelector(".pause-button").style.display = "inline";
-  document.querySelector(".play-button").style.display = "none"
-};
 
-const pauseMusic = () => {
-  music.pause()
- document.querySelector(".pause-button").style.display = "none";
-  document.querySelector(".play-button").style.display = "inline"
+function formatTime (seconds){
+  let fieldMinutes = Math.floor(seconds / 60);
+  let fieldSeconds = seconds % 60;
+  if (fieldSeconds < 10){fieldSeconds = "0" + fieldSeconds}
+  return fieldMinutes + ":" + fieldSeconds;
 }
 
-document.querySelector(".play-button").addEventListener("click", playMusic);
+const updateProgress = () => {
+  let currentTime = document.querySelector(".current-time");
+  currentTime.textContent = formatTime(Math.floor(music.currentTime));
+}
 
-document.querySelector(".pause-button").addEventListener("click", pauseMusic);
+const togglePausePlay = () => {
+  if (music.paused){
+    music.play();
+    document.querySelector(".pause-button").style.display = "inline";
+    document.querySelector(".play-button").style.display = "none";
+  }
+  else{
+    music.pause();
+    document.querySelector(".play-button").style.display = "inline";
+    document.querySelector(".pause-button").style.display = "none";
+  }
+}
+
+document.querySelector("audio").addEventListener("timeupdate", updateProgress);
+document.querySelector(".play-button").addEventListener("click", togglePausePlay);
+document.querySelector(".pause-button").addEventListener("click", togglePausePlay);
